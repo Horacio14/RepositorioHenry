3e143359a48824aa9993cec56777bb50904f7931
HRosciani <hora.rosciani@gmail.com>
2023-11-12 21:01:18 -0300

    Ejercicio Variables

---
+++ Variables.js
@@ -0,0 +1,27 @@
+//String
+
+var bootcamp = "henry";
+
+//Numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 4,5;
+
+// Booleanos
+
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+//Undefined
+
+var cajon1;
+
+console.log(cajon1);
+
+//null
+
+var sinNada = null;
