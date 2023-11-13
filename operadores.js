c73c23d160b5fa31103d5f1a533e1b1a03f4d955
HRosciani <hora.rosciani@gmail.com>
2023-11-12 23:59:47 -0300

    operadores

---
+++ operadores.js
@@ -0,0 +1,37 @@
+console.log(3 + 5);
+console.log(14 - 5);
+console.log(30 / 6);
+console.log(95 % 4);
+
+//Precedencia de operadores
+console.log(3 + 5) * (2 - 8);
+//en operaciones combinadas no se respeta la prioridad matematica,
+//sino que javascript tiene un valor para cada operador.;
+
+//Adicion y strings
+//con + concatenamos strings
+
+console.log("hola" + " ramon");
+var nombre = " ramon";
+var saludo = "hola" + nombre;
+console.log(saludo);
+
+//copiado de los apuntes online de henry
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
