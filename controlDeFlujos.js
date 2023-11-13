6bcda757e679ff93511e9c7440559b5f3026bfda
HRosciani <hora.rosciani@gmail.com>
2023-11-13 19:10:48 -0300

    ifElse

---
+++ controlDeFlujos.js
@@ -0,0 +1,12 @@
+function viajar (destino){
+    if (destino === 'brasil'){
+         console.log('gire a la izquierda');
+    }  else if (destino === 'argentina'){
+         console.log('gire a la DERECHA');
+    }   else {
+        console.log('nos perdimos');
+    };
+
+    };    
+    
+ viajar("argentiasd");
